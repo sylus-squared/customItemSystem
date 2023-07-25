f65c9b8f1ec187a76032cfc9ade1f08fd61d9622
SylusSquared7 <128179957+SylusSquared7@users.noreply.github.com>
2023-07-25 01:06:34 +0100

    Added files via upload
    git is being stupid and not letting me create a PR so I have to upload all the files :(

--- newCustomItemSystem/src/main/java/com/sylus/newcustomitemsystem/NewCustomItemSystem.java
+++ newCustomItemSystem/src/main/java/com/sylus/newcustomitemsystem/NewCustomItemSystem.java
@@ -3,10 +3,7 @@ package com.sylus.newcustomitemsystem;
 import com.sylus.newcustomitemsystem.commands.loreAddTest;
 import com.sylus.newcustomitemsystem.events.*;
 import com.sylus.newcustomitemsystem.handlers.playerHandler;
-import com.sylus.newcustomitemsystem.manager.cooldowns;
 import com.sylus.newcustomitemsystem.manager.itemManager;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NewCustomItemSystem extends JavaPlugin {
@@ -22,15 +19,9 @@ public final class NewCustomItemSystem extends JavaPlugin {
 
         new playerHandler(this);
         new itemManager();
-        new launcher().launcherHandeler(this);
-        new arrow().arrowHandeler(this);
         new menuClick().menuHandeler(this);
-        new lightningStrike().lightningHandeler(this);
-        new tntThrow().tntHandeler(this);
-        new otherLauncher().otherLauncherHandeler(this);
-        new glowEffect().glowHandeler(this);
-        new cactusThrow().cactusThrowHandeler(this);
-        new anvilRain().anvilRainHandeler(this);
+        new cactusThrow().cactusThrowHandeler(this); // Does not work RN
+        new anvilRain().anvilRainHandeler(this); // Does not work rn
 
     }
 
